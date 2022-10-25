7bad3c603a36a5a619cbb548d761968e5a892ef2
Emile Goeminne <emile.goeminne@student.howest.be>
2022-10-25 23:26:37 +0200

    first commit

---
+++ src/scripts/three.js
@@ -0,0 +1,39 @@
+/**
+ * You get the imports for free
+ */
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+
+/**
+ * Base
+ */
+
+// Canvas
+
+// Scene
+
+/**
+ * Models
+ */
+
+/**
+ * Lights
+ */
+
+
+/**
+ * Sizes
+ */
+
+/**
+ * Camera
+ */
+// Base camera
+
+// Controls
+
+/**
+ * Renderer
+ */
