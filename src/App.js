1e67844ea01e19bd71c25be61e24eb15b9ed710d
GoeminneEmile <43674282+GoeminneEmile@users.noreply.github.com>
2022-10-26 10:37:55 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,23 +4,7 @@ import './scripts/three';
 function App() {
   return (
     <div className="App">
-      <h1>Emile <span className='yellow'>Goeminne</span></h1>
-      <header className='mainHeader'>
-        <div className='mainHeader__content'>
-          <canvas className="webgl"></canvas>
-        </div>
-        <div className='mainHeader__videoContainer'>
-          <video className="mainHeader__video" muted autoPlay loop>
-            <source src="/videos/yt5s.com-Dark.mov(360p).mp4" type="video/mp4" />
-          </video>
-        </div>
-      </header>
-      <div>
-        more stuff
-      </div>
-      <div>
-        more stuff
-      </div>
+      Put canvas here
     </div>
   );
 }
